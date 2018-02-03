3c988bce7fdd2b7e167409016019667b5254e13b
ArneshMaurya <33183478+ArneshMaurya@users.noreply.github.com>
2018-02-03 12:25:50 +0530

    Create push_back.cpp

---
+++ push_back.cpp
@@ -0,0 +1,22 @@
+// C++ code to demonstrate the working of
+
+#include<iostream>
+#include<string> 
+using namespace std;
+int main()
+{
+ 
+    string str;
+ 
+ 
+    getline(cin,str);
+ 
+ 
+    str.push_back('.');
+ 
+    cout << str;
+  
+ 
+    return 0;
+ 
+}
