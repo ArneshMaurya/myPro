d1c1f21c156defab67a5f45dfba09446a8d866ef
ArneshMaurya <33183478+ArneshMaurya@users.noreply.github.com>
2018-03-18 15:33:20 +0530

    Create Pro_decr_order.cpp

---
+++ Pro_decr_order.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	
+	int n,i,j,temp,arr[10000];
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	printf("\n");
+
+	
+	for(i=0;i<n;i++){
+		for(j=0;j<n;j++)
+		if(arr[j]<arr[j+1])
+		temp=arr[j];
+		arr[j]=arr[j+1];
+		arr[j+1]=temp;
+	}
+for(i=0;i<n;i++)
+printf("%d\n",arr[i]);
+
+return 0;
+}
