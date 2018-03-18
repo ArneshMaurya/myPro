7b7a49ca5338d0114a5d5ecd3ea32ed57aa6b75b
ArneshMaurya <33183478+ArneshMaurya@users.noreply.github.com>
2018-03-18 15:50:01 +0530

    Update Pro_decr_order.cpp

--- Pro_decr_order.cpp
+++ Pro_decr_order.cpp
@@ -14,10 +14,12 @@ int main(){
 	for(i=0;i<n;i++){
 		for(j=0;j<n;j++)
 		if(arr[j]<arr[j+1])
+	{
 		temp=arr[j];
 		arr[j]=arr[j+1];
 		arr[j+1]=temp;
 	}
+}
 for(i=0;i<n;i++)
 printf("%d\n",arr[i]);
 
